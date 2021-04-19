3c6b931d03cc511e1a1c63d9dcc4fd4016f4a91c
BloodLind <pashtet.sadist@gmail.com>
2021-04-19 21:36:25 +0300

    Add Xamarin Base

---
+++ MusicPlayer/MusicPlayer.Xamarin/MusicPlayer.Xamarin.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Views;
+using MvvmCross.Forms.Platforms.Android.Core;
+
+namespace MusicPlayer.Xamarin.Droid
+{
+    [Activity(Label = "MusicPlayer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Core.App, App>, Core.App, App>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(bundle);
+        }
+
+    }
+}
