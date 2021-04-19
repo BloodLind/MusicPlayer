b3db82dd92da0ad64e840aed372ec7f1b3daa30b
BloodLind <pashtet.sadist@gmail.com>
2021-04-19 20:03:38 +0300

    Project Core created

---
+++ MusicPlayer/MusicPlayer.Core/CoreApp.cs
@@ -0,0 +1,19 @@
+﻿
+using MusicPlayer.Core.ViewModels;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Core
+{
+    public class CoreApp : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
