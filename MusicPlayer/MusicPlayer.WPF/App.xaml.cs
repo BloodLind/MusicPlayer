12fa46826befd163c80c0bc51f76f8487fa58c0a
BloodLind <pashtet.sadist@gmail.com>
2021-04-19 21:07:36 +0300

    add wpf project

---
+++ MusicPlayer/MusicPlayer.WPF/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.Platforms.Wpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MvvmCross.Core;
+
+namespace MusicPlayer.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        public App()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.CoreApp>>();
+        }
+    }
+}
