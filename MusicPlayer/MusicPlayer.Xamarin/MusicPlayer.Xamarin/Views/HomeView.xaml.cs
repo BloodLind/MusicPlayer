d3db5b8dc9caf3d23f86ab3c646644000ab383b8
BloodLind <pashtet.sadist@gmail.com>
2021-04-22 00:47:54 +0300

    Xamarin Home Page Created and added default images

--- MusicPlayer/MusicPlayer.Xamarin/MusicPlayer.Xamarin/Views/HomeView.xaml.cs
+++ MusicPlayer/MusicPlayer.Xamarin/MusicPlayer.Xamarin/Views/HomeView.xaml.cs
@@ -10,12 +10,58 @@ using Xamarin.Forms.Xaml;
 
 namespace MusicPlayer.Xamarin.Views
 {
+    enum LabelsSize
+    {
+        Selected = 24,
+        Unselected = 18
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeView : MvxContentPage
     {
+        private Label selectedTab;
+        private List<Label> labelTabs;
+
+        private void SetUnselectedLabel()
+        {
+            int unselected = (int)LabelsSize.Unselected;
+            queueLabel.FontSize = unselected;
+            playlistsLabel.FontSize = unselected;
+            albumsLabel.FontSize = unselected;
+            artistsLabel.FontSize = unselected;
+            tracksLabel.FontSize = unselected;
+        }
         public HomeView()
         {
             InitializeComponent();
+            selectedTab = tracksLabel;
+            labelTabs = new List<Label> { queueLabel, playlistsLabel, tracksLabel, albumsLabel, artistsLabel };
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var tappedObject = (Label)sender;
+            selectedTab = tappedObject;
+            SetUnselectedLabel();
+            selectedTab.FontSize = (int)LabelsSize.Selected;
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            int currentIndex = labelTabs.IndexOf(selectedTab);
+           if(e.Direction == SwipeDirection.Right)
+            {
+                selectedTab = currentIndex > 0 ? labelTabs[currentIndex - 1] : selectedTab;
+                SetUnselectedLabel();
+                selectedTab.FontSize = (int)LabelsSize.Selected;
+
+            }
+           else if (e.Direction == SwipeDirection.Left)
+            {
+                SetUnselectedLabel();
+                selectedTab.FontSize = (int)LabelsSize.Selected;
+                selectedTab = currentIndex < labelTabs.Count - 1 ? labelTabs[currentIndex + 1] : selectedTab;
+            }
         }
     }
 }
